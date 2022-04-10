77d36ae75c47fe13878ecf478d5a39e1d1d77d5b
polymahh <otman.elkantaoui@gmail.com>
2022-04-10 14:37:27 +0000

    fixed updating read state

--- script.js
+++ script.js
@@ -9,6 +9,7 @@
       this.pages = pages;
       this.state = state
     }
+    
   }
   const DEFAULT_DATA = [
     { title: "The Lord of the Rings", author: "Tolkien",pages:"343", state: "read" },
@@ -102,6 +103,7 @@ let library = {
 
     let  stateButton = document.createElement('button');
     stateButton.classList.add('state')
+    stateButton.setAttribute('id',num.title)
     stateButton.innerText = num.state;
     let del = document.createElement('button');
     del.classList.add('delete')
@@ -117,9 +119,20 @@ let library = {
   
   },
   switchState (event){
+    let book = this.myLibrary.find(item => item.title == event.target.id);
+    let bookIndex = this.myLibrary.indexOf(book);
+      
     if (event.target.innerText == 'READ'){
-      event.target.innerText = 'NOT READ';
-    }else event.target.innerText = 'READ';
+      console.log(this.myLibrary)
+      this.myLibrary[bookIndex].state = 'not read' ;
+      this.updateLocalStorage();
+      this.render();
+    }else {
+
+      this.myLibrary[bookIndex].state = 'read' ;
+      this.updateLocalStorage();
+      this.render();
+    }
   },
   
   updateLocalStorage() {
@@ -143,85 +156,4 @@ let library = {
 library.init();
 
 })();
-/*
-let container = document.querySelector('.container') ;
-let myLibrary = [];
 
-show();
-const addButton = document.querySelector('#add');
-addButton.addEventListener('click' ,addBookToLibrary);
-
-  let Book = function (title, author, pages, status ){
-      this.title = title;
-      this.author = author;
-      this.pages = pages;
-      this.status = status ;
-      
-  }
-
-
-  function addBookToLibrary() {
-
-    let title = document.querySelector('#title').value;
-    console.log(title);
-    let author = document.querySelector('#author').value;
-    let pages = document.querySelector('#pages').value;
-    let status = document.querySelector('#status').value;
-    let bk = new Book(title, author, pages, status);
-    myLibrary.push(bk);
-    show();
-
-  }
-
-  function show () {
-    while(container.childNodes[0]){container.removeChild(container.childNodes[0])}
-    myLibrary.forEach(showBook);
-    deleteFun ();
-  
-  }
-
-
-  function showBook (num) {
-     
-      let sub = document.createElement('div')
-      sub.classList.add('book')
-      container.appendChild(sub);
-
-      let divTitle = document.createElement('div');
-      divTitle.textContent = num.title;
-      let divAuthor = document.createElement('div');
-      divAuthor.textContent = num.author;
-      let divPages = document.createElement('div');
-      divPages.textContent = num.pages;
-      console.log(num)
-      let del = document.createElement('button');
-      del.classList.add('delete')
-      del.setAttribute('id',num.title)
-      del.textContent = 'Delete';
-
-      sub.appendChild(divTitle);
-      sub.appendChild(divAuthor);
-      sub.appendChild(divPages);
-      sub.appendChild(del);
-    
-  }
-
-
-function deleteFun () {
-  let delButtons = document.querySelectorAll('.delete')
-  delButtons.forEach((delButton) => {
-    delButton.addEventListener('click', () => {
-          let res = myLibrary.find(item => item.title == delButton.id);
-          console.log(res);
-          let resNum = myLibrary.indexOf(res);
-          console.log(resNum);
-          myLibrary.splice(resNum,1) ;
-          console.log(myLibrary)
-          
-          show();
-    });
-  });
-}
-
-//test
-*/
